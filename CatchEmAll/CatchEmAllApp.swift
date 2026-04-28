d655a8e128cca4325e8aabcfc891a2464dc16c7d
Jim Walejko <jwalejko@me.com>
2026-04-27 20:41:29 -0700

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Jim Walejko on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
